4ba6b91e57c65e05ef793ca3b4f7ae00e2c8d750
Bigeard Antoine <antoine@vals.ai>
2025-04-23 17:18:40 -0700

    update for sdk

--- custom_model_corp_fin_dataset.py
+++ custom_model_corp_fin_dataset.py
@@ -10,8 +10,37 @@ import os
 import asyncio
 from vals import Suite, Run, RunParameters
 
-openai_client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-google_client = google.genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+provider_args = {
+    "openai": {"api_key": os.getenv("OPENAI_API_KEY")},
+    "google": {
+        "api_key": os.getenv("GOOGLE_API_KEY"),
+        "base_url": "https://generativelanguage.googleapis.com/v1beta/openai/",
+    },
+    "anthropic": {
+        "api_key": os.getenv("ANTHROPIC_API_KEY"),
+        "base_url": "https://api.anthropic.com/v1/",
+    },
+    "together": {
+        "api_key": os.getenv("TOGETHER_API_KEY"),
+        "base_url": "https://api.together.xyz/v1/",
+    },
+    "fireworks": {
+        "api_key": os.getenv("FIREWORKS_API_KEY"),
+        "base_url": "https://api.fireworks.ai/v1/",
+    },
+    "mistralai": {
+        "api_key": os.getenv("MISTRAL_API_KEY"),
+        "base_url": "https://api.mistral.ai/v1/",
+    },
+    "grok": {
+        "api_key": os.getenv("GROK_API_KEY"),
+        "base_url": "https://api.x.ai/v1",
+    },
+    "cohere": {
+        "api_key": os.getenv("COHERE_API_KEY"),
+        "base_url": "https://api.cohere.ai/compatibility/v1/",
+    },
+}
 
 INSTRUCTION = """
 I will give you a question and a document.
@@ -31,7 +60,7 @@ Your answer:
 
 
 def get_doc_type(model_name):
-    if "gpt-4o" in model_name:
+    if "gpt-4o" or "Llama-3.3-70B-Instruct-Turbo" in model_name:
         return "trimmed_openai"
     elif "claude" in model_name:
         return "trimmed_anthropic"
@@ -57,55 +86,29 @@ def get_doc(model_name, files):
 
     if "gpt-4o" in model_name:
         encoding = tiktoken.encoding_for_model("gpt-4o")
-        tokens = encoding.encode(document)[:120000]
+        tokens = encoding.encode(document)[:125000]
         return encoding.decode(tokens)
 
     return document
 
 
 async def call_model(
-    model_key, provider, prompt, temperature, max_tokens, system_prompt=None
+    client, model_key, prompt, temperature, max_tokens, system_prompt=None
 ):
-    if provider == "google":
-        generation_config = {
-            "temperature": temperature,
-            "max_output_tokens": max_tokens,
-        }
-        if system_prompt is not None and len(system_prompt) > 0:
-            generation_config["system_instruction"] = system_prompt
+    messages = []
+    if system_prompt is not None and len(system_prompt) > 0:
+        messages.append({"role": "system", "content": system_prompt})
+    messages.append({"role": "user", "content": prompt})
 
-        return await google_client.aio.models.generate_content(
-            model=model_key,
-            contents=[prompt],
-            config=generation_config,
-        )
-
-    elif provider == "openai":
-        messages = []
-        if system_prompt is not None and len(system_prompt) > 0:
-            messages.append({"role": "system", "content": system_prompt})
-        messages.append({"role": "user", "content": prompt})
-
-        return await openai_client.chat.completions.create(
-            model=model_key,
-            messages=messages,
-            temperature=temperature,
-            max_tokens=max_tokens,
-        )
-
-    else:
-        raise ValueError(f"Provider {provider} not supported")
+    return await client.chat.completions.create(
+        model=model_key,
+        messages=messages,
+        temperature=temperature,
+        max_tokens=max_tokens,
+    )
 
 
-def read_output(provider, output):
-    if provider == "google":
-        return {
-            "llm_output": output.text,
-            "metadata": {
-                "in_tokens": output.usage_metadata.prompt_token_count,
-                "out_tokens": output.usage_metadata.candidates_token_count,
-            },
-        }
+def read_output(output):
     return {
         "llm_output": output.choices[0].message.content,
         "metadata": {
@@ -119,7 +122,8 @@ def get_custom_model(model_name, parameters, *args, **kwargs):
     system_prompt = parameters.get("system_prompt", "")
     temperature = parameters.get("temperature", 0)
     max_tokens = parameters.get("max_output_tokens", 512)
-    provider, model_key = model_name.split("/")
+    provider, model_key = model_name.split("/", 1)
+    client = AsyncOpenAI(**provider_args[provider])
 
     async def custom_call(
         test_input: str, files: dict[str, BytesIO], context: dict[str, any]
@@ -129,9 +133,9 @@ def get_custom_model(model_name, parameters, *args, **kwargs):
 
         try:
             output = await call_model(
-                model_key, provider, prompt, temperature, max_tokens, system_prompt
+                client, model_key, prompt, temperature, max_tokens, system_prompt
             )
-            out = read_output(provider, output)
+            out = read_output(output)
             return out
         except Exception as e:
             print(e)
@@ -141,20 +145,19 @@ def get_custom_model(model_name, parameters, *args, **kwargs):
 
 
 if __name__ == "__main__":
-
     async def main():
         # Replace the mapping_suite_ids with the mapping we provided you by email.
         mapping_suite_ids = {}
 
-        task = "max_fitting_context_task"
-        model_under_test = "google/gemini-1.5-pro-002"
-        eval_model = "openai/gpt-4o"
+        task = "shared_max_context"
+        model_under_test = "together/meta-llama/Llama-3.3-70B-Instruct-Turbo"
+        eval_model = "anthropic/claude-3-5-sonnet-20241022"
 
         parameters = RunParameters(
             eval_model=eval_model,
             temperature=0,
             max_output_tokens=1024,
-            parallelism=20,
+            parallelism=2,
         )
 
         custom_model = get_custom_model(model_under_test, parameters.model_dump())
@@ -164,11 +167,11 @@ if __name__ == "__main__":
             model=custom_model,
             model_name=model_under_test,
             parameters=parameters,
-            upload_concurrency=10,
+            upload_concurrency=3,
         )
 
         # To resume a run that crashed
         # run = await Run.from_id("run_id")
-        # await run.resume_run(model=custom_model, parameters=parameters, upload_concurrency=5)
+        # await run.resume_run(model=custom_model)
 
     asyncio.run(main())
